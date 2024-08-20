ebc0f0372af2deb6e1b414cf62c03372e7e65ec0
monzondeveloper <monzondeveloper@gmail.com>
2024-08-20 18:43:08 -0300

    commit of consumer

---
+++ AzureServiceBus.Consumer/Program.cs
@@ -0,0 +1,48 @@
+﻿using Azure.Messaging.ServiceBus;
+
+const string SERVICE_BUS_CONN_STRING = "your_conn_string_here";
+const string QUEUE_NAME = "az-queue-name";
+
+ServiceBusClient client;
+ServiceBusProcessor processor = default!;
+
+async Task MessageHandler(ProcessMessageEventArgs processMessageEventArgs)
+{
+    string body = processMessageEventArgs.Message.Body.ToString();
+    Console.WriteLine(body);
+
+    await processMessageEventArgs.CompleteMessageAsync(processMessageEventArgs.Message);
+}
+
+Task ErrorHandler(ProcessErrorEventArgs processErrorEventArgs)
+{
+    Console.WriteLine(processErrorEventArgs.Exception.ToString());
+    return Task.CompletedTask;
+}
+
+client = new ServiceBusClient(SERVICE_BUS_CONN_STRING);
+processor = client.CreateProcessor(QUEUE_NAME, new ServiceBusProcessorOptions());
+
+try
+{
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    await processor.StartProcessingAsync();
+
+    Console.WriteLine("Press any key to end the processing");
+    Console.ReadKey();
+
+    Console.WriteLine("\nStopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("\nStopped receiving messages");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception: {ex.Message}");
+}
+finally
+{
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
---
+++ AzureServiceBus.Sender/Program.cs
@@ -0,0 +1,39 @@
+﻿using Azure.Messaging.ServiceBus;
+
+const string SERVICE_BUS_CONN_STRING = "your_conn_string_here";
+const string QUEUE_NAME = "az-queue-name";
+const int MAX_NUMBER_OF_MESSAGES = 10;
+
+ServiceBusClient client;
+ServiceBusSender sender;
+
+client = new ServiceBusClient(SERVICE_BUS_CONN_STRING);
+sender = client.CreateSender(QUEUE_NAME);
+
+using ServiceBusMessageBatch batch = await sender.CreateMessageBatchAsync();
+for (int i = 0; i < MAX_NUMBER_OF_MESSAGES; i++)
+{
+    if (!batch.TryAddMessage(new ServiceBusMessage($"This is a message - {i}")))
+    {
+        Console.WriteLine($"Message - {i} was not added!");
+    }
+}
+
+try
+{
+    await sender.SendMessagesAsync(batch);
+    Console.WriteLine("Messages sent");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception: {ex.Message}");
+    throw;
+}
+finally
+{
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+
+
